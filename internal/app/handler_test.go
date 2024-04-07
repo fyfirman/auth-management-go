613a9239f07a3735d0a00dba11e47c0ec017a202
Firmansyah Yanuar <fyfirman@gmail.com>
2024-04-08 05:33:20 +0700

    test: add 400 for invalid request

--- internal/app/handler_test.go
+++ internal/app/handler_test.go
@@ -32,7 +32,7 @@ func TestUserHandler_Register(t *testing.T) {
 
 	mockUserService.Mock.On("RegisterUser", ctx, mock.AnythingOfType("*dto.RegisterRequest")).Return(response, nil)
 	// Create a new HTTP request
-	reqBody := []byte(`{"username": "testuser", "email": "testuser@gmail.com", "password": "testpassword"}`)
+	reqBody := []byte(`{"username": "testuser", "email": "testuser@gmail.com", "password": "testpassword", "role": "superadmin"}`)
 	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(reqBody))
 	if err != nil {
 		t.Fatal(err)
@@ -58,4 +58,48 @@ func TestUserHandler_Register(t *testing.T) {
 	if !isSame || err != nil {
 		t.Errorf("expected response body %s, got %s", expectedResp, resultBody)
 	}
+}
+
+func TestUserHandler_Register_InvalidRequest(t *testing.T) {
+	mockUserService := new(mocks.UserServiceInterface)
+
+	ctx := context.Background()
+	// Create a new instance of UserHandler
+	handler := app.NewUserHandler(mockUserService)
+
+	response := &dto.RegisterResponse{
+		ID:        12345,
+		Username:  "john_doe",
+		Email:     "john_doe@example.com",
+		CreatedAt: time.Date(2024, 4, 8, 10, 0, 0, 0, time.UTC),
+		UpdatedAt: time.Date(2024, 4, 8, 12, 0, 0, 0, time.UTC),
+	}
+
+	mockUserService.Mock.On("RegisterUser", ctx, mock.AnythingOfType("*dto.RegisterRequest")).Return(response, nil)
+	// Create a new HTTP request
+	reqBody := []byte(`{"username": "testuser", "email": "testuser@gmail.com", "password": "testpassword", "role": "hack!!!"}`)
+	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new HTTP response recorder
+	recorder := httptest.NewRecorder()
+
+	// Call the Register method
+	handler.Register(recorder, req)
+
+	mockUserService.Mock.AssertNotCalled(t, "RegisterUser")
+	// Check the response status code
+	if recorder.Code != http.StatusBadRequest {
+		t.Errorf("expected status code %d, got %d", http.StatusBadRequest, recorder.Code)
+	}
+
+	// Check the response body
+	// expectedResp := `{"id":12345,"username":"john_doe","email":"john_doe@example.com","created_at":"2024-04-08T10:00:00Z","updated_at":"2024-04-08T12:00:00Z"}`
+	// resultBody := recorder.Body.String()
+	// isSame, err := pkg.CompareJSONMaps(resultBody, expectedResp)
+	// if !isSame || err != nil {
+	// 	t.Errorf("expected response body %s, got %s", expectedResp, resultBody)
+	// }
 }
